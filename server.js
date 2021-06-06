021464f577d803963a7ec402975d38bd1e8dc678
Satyam <Naithani>
2021-06-06 17:41:29 +0530

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 const dotenv = require('dotenv').config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
